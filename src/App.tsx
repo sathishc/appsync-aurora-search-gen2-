7f4ed2dfe9031f717a128d77aba3e64baf008660
sathishc <sat.hariharan@gmail.com>
2024-06-14 15:45:26 +0530

    remove frontend auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,12 @@ import { useEffect } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import '@aws-amplify/ui-react/styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
 
 
 const client = generateClient<Schema>({authMode:'apiKey'});
 
 function App() {
   // const [searchString, setSearchString] = useState("")
-
-  const modelSchema = `Product{
-    product_id: a.string().required(),
-    product_name: a.string().required(),
-    category: a.string(),
-    discounted_price: a.string(),
-    actual_price: a.string(),
-    discount_percentage: a.string(),
-    rating: a.string(),
-    rating_count: a.string(),
-    about_product: a.string(),
-  }`
 
   useEffect(() => {
     console.log(client);    
@@ -31,23 +18,13 @@ function App() {
     client.queries.search({query:"Anker"})
   }
 
-  /*
-  async function addProduct() {
-    const promptString = "Generate a record for the following graphql schema. " + modelSchema
-    const product = await client.mutations.addProduct({"query":promptString});
-    console.log(product);
-  }*/
 
   return (
-    <Authenticator>
-      {() => (
     <main>
       <h1>My Products</h1>
       <button onClick={search}>Add Product</button>
     
     </main>
-     )}
-    </Authenticator>
   );
 }
 
